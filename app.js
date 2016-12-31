c828ddb55a063294babf1a20ab7dae2f023786bd
SirRobZ <sazarapkar@gmail.com>
2016-12-31 15:44:55 -0500

    Updated

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
 function main() {
-	var state = {};
+	var state = {
+		questions: [
+			{
+				question: 'What is the longest river in the world?',
+				answers: ['Amazon','Nile','Yangtze','Mississippi'],
+				answer: 'Amazon'
+			},
+			{
+				question: 'What is the highest peak in North America?',
+				answers: ['Mount McKinley','Mount Logan','Pico de Orizaba','Mount Saint Elias'],
+				answer: 'Mount McKinley'
+			}
+		],
+		currentQuestion: 0
+	};
+
+	function renderQuestion(state){
+		//based on Current question index retrieve question from questions array
+		var index = state.currentQuestion;
+		var question = state.questions[index]
+		var answerListHTML = question.answers.map(function(answer, index){
+			return('<li>'+
+						'<input type="radio" name="answer" value="' + answer + '" id="answer ' + index + '">'+
+						'<label for="answer ' + index + '">' + answer + '</label>'+
+					'</li>')
+		})
+		$('.container .question').text(question.question);
+		$('.container ul').html(answerListHTML);
+	}
+
+	function bindStartButtonEvent() {
+		$('.start button').on('click', function(event){
+			$('.start').toggleClass('hidden');
+			$('.container').toggleClass('hidden');
+		})
+	}
+
+	bindStartButtonEvent();
+	renderQuestion(state);
 }
 $(document).ready(main);
